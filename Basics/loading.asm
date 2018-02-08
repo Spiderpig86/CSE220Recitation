2d41fe2d8e05c28eb5fc1dde7eddeefc10e17a25
spiderpig86 <slim679975@gmail.com>
2018-02-08 13:38:24 -0500

    feat(loading.asm): Added more examples of loading

--- Basics/loading.asm
+++ Basics/loading.asm
@@ -19,5 +19,58 @@
     lbl_lw: .asciiz "lw: "
     lbl_lb: .asciiz "lb: "
     lbl_lh: .asciiz "lh: "
+    newline: .asciiz "\n"
+
+    .macro nl()
+        la $a0, newline
+        li $v0, 4
+        syscall
+    .end_macro
+
+    .macro print_la()
+        la $a0, newline
+        li $v0, 4
+        syscall
+    .end_macro
+    
+    .macro print_lw()
+        la $a0, newline
+        li $v0, 4
+        syscall
+    .end_macro
+
+    .macro print_lb()
+        la $a0, newline
+        li $v0, 4
+        syscall
+    .end_macro
+
+    .macro print_lh()
+
+    .end_macro
 .text
 
+    ### LOADING STRINGS ###
+    # Load a string in MIPS, first you must load the location of the string in memory, or the memory address.
+    la $a0, load_str # This line will load the address of where the string is located in memory
+    li $v0, 4 # Load the syscall for printing a string
+    syscall # Actually print the string
+
+    nl()
+
+    # We can load a single character of a string using the lb or load byte instruction, since a character is essentially a single byte.
+    la $a0, load_str
+    lb $t0, 5($a0) # Load the 5th carhacter of the string which is 'e'
+    move $a0, $t0 # Copy the value of the char to $a0 for printing
+    li $v0, 11 # Syscall 11 for printing a char
+    syscall
+
+    nl()
+
+    # For the string that contains numbers, it is all treated as a regular character
+    la $a0, load_str_num
+    lb $a0, 1($a0) # Loads the char '2' in the string
+    li $v0, 1
+    syscall
+    
+    nl()
