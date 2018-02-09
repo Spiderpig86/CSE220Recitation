4e4ab451d9ee3bb56ebc66f83d6322bd711abc5e
spiderpig86 <slim679975@gmail.com>
2018-02-09 01:14:31 -0500

    feat(loading.asm): Finished examples demonstrating how to load values from memory

--- Basics/loading.asm
+++ Basics/loading.asm
@@ -28,30 +28,33 @@
     .end_macro
 
     .macro print_la()
-        la $a0, newline
+        la $a0, lbl_la
         li $v0, 4
         syscall
     .end_macro
     
     .macro print_lw()
-        la $a0, newline
+        la $a0, lbl_lw
         li $v0, 4
         syscall
     .end_macro
 
     .macro print_lb()
-        la $a0, newline
+        la $a0, lbl_lb
         li $v0, 4
         syscall
     .end_macro
 
     .macro print_lh()
-
+        la $a0, lbl_lh
+        li $v0, 4
+        syscall
     .end_macro
 .text
 
     ### LOADING STRINGS ###
     # Load a string in MIPS, first you must load the location of the string in memory, or the memory address.
+    print_la()
     la $a0, load_str # This line will load the address of where the string is located in memory
     li $v0, 4 # Load the syscall for printing a string
     syscall # Actually print the string
@@ -59,6 +62,7 @@
     nl()
 
     # We can load a single character of a string using the lb or load byte instruction, since a character is essentially a single byte.
+    print_lb()
     la $a0, load_str
     lb $t0, 5($a0) # Load the 5th carhacter of the string which is 'e'
     move $a0, $t0 # Copy the value of the char to $a0 for printing
@@ -68,9 +72,82 @@
     nl()
 
     # For the string that contains numbers, it is all treated as a regular character
+    print_lb()
     la $a0, load_str_num
     lb $a0, 1($a0) # Loads the char '2' in the string
-    li $v0, 1
+    li $v0, 11 # Note that this is also 50 in ASCII
     syscall
     
     nl()
+
+    ### LOADING NUMBERS ###
+    # Loading numbers is a bit different as we do not need to load the address directly. Since it is a single value unlike a string (multiple chars), we can simply just load the word value
+    print_lw()
+    lw $a0, load_word # Loads 420 to $a0
+    la $t0, load_word # If we use la, we load the address of where the number is located in memory
+    li $v0, 1
+    syscall
+
+    nl()
+    
+    # Unlike strngs, we will not be able to load the inividual digits of a given number. Instead, we must use bit masking in binary to do that.
+
+    # Looking at the hex number, we can easily load each of these values. Since a word is 32 bits, this means that we can load 4 different bytes in it
+    print_lb()
+    la $t0, load_word_hex # Load the address of hex int, lw will not work
+    lb $a0, 0($t0) # Load the first byte of the number, or the LSB on the right side
+    li $v0, 34 # Syscall for printing out hex
+    syscall
+
+    nl()
+
+    print_lb()
+    la $t0, load_word_hex # Load the address of hex int, lw will not work
+    lb $a0, 1($t0) # Load the first byte of the number, or the LSB on the right side
+    li $v0, 34 # Syscall for printing out hex
+    syscall
+
+    nl()
+
+    print_lb()
+    la $t0, load_word_hex # Load the address of hex int, lw will not work
+    lb $a0, 2($t0) # Load the first byte of the number, or the LSB on the right side
+    li $v0, 34 # Syscall for printing out hex
+    syscall
+
+    nl()
+
+    print_lb()
+    la $t0, load_word_hex # Load the address of hex int, lw will not work
+    lb $a0, 3($t0) # Load the first byte of the number, or the LSB on the right side
+    li $v0, 34 # Syscall for printing out hex
+    syscall
+
+    nl()
+
+    # For loading an array, it essentially has the same idea as the example above. We need to load the addres (not lw since that just gives us the value, not where it is located in memory)
+    # Then, the offsets must be in multiples of 4 since the size of an int is 4 bytes.
+    print_lw()
+    la $t0, load_word_arr # Load the address of the number array
+    lw $a0, ($t0) # Load the first number
+    li $v0, 1
+    syscall
+    nl()
+
+    # Now we can print the other elements by changing the offsets
+    print_lw()
+    lw $a0, 4($t0) # Load the first number
+    li $v0, 1
+    syscall
+    nl()
+
+    print_lw()
+    lw $a0, 8($t0) # Load the first number
+    li $v0, 1
+    syscall
+    nl()
+
+    print_lw()
+    lw $a0, 12($t0) # Load the first number
+    li $v0, 1
+    syscall
